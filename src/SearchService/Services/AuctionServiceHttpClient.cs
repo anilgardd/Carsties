92676169652ff797eab3aedc3c96214c0b045e64
Anıl AKGÜN <anil@kayaport.local>
2025-11-01 23:18:27 +0300

    end of section 3

---
+++ src/SearchService/Services/AuctionServiceHttpClient.cs
@@ -0,0 +1,28 @@
+using System.Runtime.CompilerServices;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService;
+
+public class AuctionServiceHttpClient
+{
+
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
+    public AuctionServiceHttpClient(HttpClient httpClient, IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Item>> GetItemsForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, string>()
+        .Sort(x => x.Descending(x => x.UpdatedAt))
+        .Project(x => x.UpdatedAt.ToString())
+        .ExecuteFirstAsync();
+
+        return await _httpClient.GetFromJsonAsync<List<Item>>(_configuration["AuctionServiceUrl"]
+        + "/api/auctions?date=" + lastUpdated);
+    }
+}
